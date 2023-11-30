d7f281f48dadd8536e4e8565ea31b16a132042c6
shafique5930 <shafique5930@gmail.com>
2023-11-30 19:37:11 +0500

    assignment first commit

---
+++ Assignment-01/Read.md
@@ -0,0 +1,68 @@
+## **Task 1:**
+
+**Task Description:**
+
+1. **Scenario:**
+You are tasked to simulate a simple shopping experience using conditional statements and loops at a supermarket.
+2. **Requirements:**
+    - Create a TypeScript program that simulates a shopping scenario with multiple conditions.
+    - Utilize if-else, if-else if-else statements for decision making based on different scenarios encountered while shopping.
+    - Incorporate nested loops to simulate actions within various sections of the supermarket.
+3. **Functionalities to Include:**
+    - **Scenario 1 - Buying Groceries:**
+        - Simulate buying fruits, vegetables, and groceries.
+        - Apply if-else statements to check specific conditions:
+            - Check if the customer wants fruits or vegetables.
+            - Based on the choice, suggest different items available and their prices.
+            - Display the total bill.
+    - **Scenario 2 - Checking Discounts:**
+        - Introduce a discount scenario based on the total bill amount.
+        - Use if-else if-else statements to apply discounts:
+            - If the bill amount is above a certain value, apply a discount.
+            - Display the discounted total.
+    - **Scenario 3 - Checkout Process:**
+        - Simulate the checkout process.
+        - Use nested loops to ask for payment methods:
+            - First, ask the user for the preferred payment method (nested loop for payment options).
+            - Display a confirmation message based on the payment method chosen.
+
+## Task 2:
+
+- **Scenario 1: Sum numbers**
+    - Write a function named **`add`** that takes two numbers as arguments and returns their sum.
+- **Scenario 2: Check Even or Odd**
+    - Develop a function **`checkEvenOrOdd`** that checks if a given number is even or odd.
+    - The function should take an integer as a parameter and return a string indicating whether the number is even or odd.
+- **Scenario 3: Calculate Area:**
+    - Create a function **`calculateArea`** that calculates the area of a rectangle.
+    - The function should take width and height as parameters and return the calculated area.
+- **Scenario 4: String Reversal**
+    - Implement a function **`reverseString`** that reverses a given string.
+    - The function should take a string as a parameter and return the reversed string.
+- **Scenario 5: Temperature Conversion:**
+    - Create a function **`convertCelsiusToFahrenheit`** that converts temperature from Celsius to Fahrenheit.
+    - The function should take a temperature value in Celsius as a parameter and return the equivalent temperature in Fahrenheit.
+
+## Task 3:
+
+- **Scenario 1 - Modify Array with Methods:**
+    - Initialize an array with some initial elements.
+    - Demonstrate the usage of the following array methods:
+        - **`push`**: Add new elements to the end of the array.
+        - **`pop`**: Remove the last element from the array.
+        - **`shift`**: Remove the first element from the array.
+        - **`unshift`**: Add new elements to the beginning of the array.
+- **Scenario 2 - Subarray Creation:**
+    - Implement the use of **`splice`** and **`slice`** to create subarrays from the original array:
+        - **`splice`**: Create a subarray by removing elements from the original array.
+        - **`slice`**: Create a subarray without modifying the original array.
+
+**Guidelines:**
+
+- Incorporate comments to explain the logic behind each condition, loop, function or array
+- Test your program with various inputs to ensure it responds accurately to different scenarios.
+
+**Submission Guidelines:**
+
+- Submit the TypeScript file containing your program.
+- Include a brief explanation of how your program works and any challenges faced during the implementation.
--- Assignment-01/index.js
+++ Assignment-01/index.js
@@ -1,2 +1,178 @@
-console.log("hello world");
-export {};
+import inquirer from "inquirer";
+let Cart;
+let loop = true;
+const answer = await inquirer.prompt([
+    {
+        type: "list",
+        name: "options",
+        message: "Dear customer, what would you like to buy?",
+        choices: ["groceries", "vegetables", "fruits"],
+    },
+]);
+console.log(answer);
+const shoppingCart = [];
+while (loop) {
+    if (answer.options == "groceries") {
+        const grocers = await inquirer.prompt([
+            {
+                type: "list",
+                name: "groceries",
+                message: "Here is the list of Available groceries?",
+                choices: [`soap: 250`, `Suger: ${90}`, `tea: ${450}`],
+            },
+            {
+                type: "confirm",
+                name: "addmore",
+                message: "Do you want to add more",
+                default: false,
+            },
+        ]);
+        const { groceries, addmore } = grocers;
+        shoppingCart.push(groceries);
+        loop = addmore;
+    }
+    else if (answer.options == "fruits") {
+        const fruits = await inquirer.prompt([
+            {
+                type: "list",
+                name: "Fruits",
+                message: "Here is the list of Available Fruits",
+                choices: ["banana:150", "apple:280", "kiwi:500", "oranges:250"],
+            },
+            {
+                type: "confirm",
+                name: "addmore",
+                message: "Do you want to add more",
+                default: false,
+            },
+        ]);
+        const { Fruits, addmore } = fruits;
+        shoppingCart.push(Fruits);
+        loop = addmore;
+    }
+    else if (answer.options == "vegetables") {
+        const veggies = await inquirer.prompt([
+            {
+                type: "list",
+                name: "Vegetables",
+                message: "Here is the list of Available Groceries",
+                choices: ["potato:50", "tomato:30", "spinich:25", "chilies:20"],
+            },
+            {
+                type: "confirm",
+                name: "addmore",
+                message: "Do you want to add more",
+                default: false,
+            },
+        ]);
+        const { Vegetables, addmore } = veggies;
+        shoppingCart.push(Vegetables);
+        loop = addmore;
+    }
+}
+let grandTotal = [];
+let sum = 0;
+for (let x in shoppingCart) {
+    let trim = shoppingCart[x].search(":");
+    let value = shoppingCart[x].slice(trim + 1);
+    grandTotal.push(parseInt(value));
+}
+grandTotal.forEach((num) => {
+    sum += num;
+});
+let discounted = (sum * 5) / 100;
+if (sum > 100) {
+    sum = sum - discounted;
+}
+let number = 100 - (100 * 5) / 100;
+console.log(shoppingCart);
+console.log(`Total Items: ${grandTotal.length} Item(s)\nGrand Total: Rs ${sum}.00\nTotal Discout: Rs ${discounted}`);
+const checkOut = await inquirer.prompt([
+    {
+        type: "list",
+        name: "PaymentMethod",
+        message: "What would be your prefferred Payment method",
+        choices: ["Cash", "POS", "EasyPaisa", "Mater Card"],
+    },
+]);
+if (checkOut.PaymentMethod == "POS") {
+    console.log("Please Provide Your debit / credit Card");
+}
+else if (checkOut.PaymentMethod == "EasyPaisa") {
+    console.log(`Please deposit a sum of Rs.${sum} to our EasyPaisa Account Number +923321235456 `);
+}
+else if (checkOut.PaymentMethod == "Master Card") {
+    console.log("Sorry this Payment method is not available yet");
+}
+else if (checkOut.PaymentMethod == "Cash") {
+    console.log(`Thanks for depositing a bill of Rs. ${sum} in cash`);
+}
+console.log(`Thank you for shopping with us, Please visit again...`);
+console.log("Task 2");
+console.log("Scenario 1: Sum numbers");
+let add = (num1, num2) => {
+    return num1 + num2;
+};
+console.log(add(15, 25));
+console.log("Scenario 2 - Check Even or Odd");
+let checkEvenOrOdd = (num) => {
+    if (num % 2 == 0) {
+        return `The number ${num} is even`;
+    }
+    else {
+        return `The number ${num} is odd`;
+    }
+};
+console.log(checkEvenOrOdd(5));
+console.log("Scenario 3: Calculate Area");
+let calculateArea = (width, height) => {
+    return width * height;
+};
+console.log(calculateArea(5, 5));
+console.log("Scenario 4: String Reversal");
+let originalString = "Hello World";
+let reverseString = (str) => {
+    let newStr = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        newStr += str[i];
+    }
+    return newStr;
+};
+console.log(reverseString("Hello World"));
+console.log("Sencario 5: Temperature Conversion");
+let convertCelsiusToFahrenheit = (celciusTemp) => {
+    let ferheneit = (celciusTemp * 9) / 5 + 32;
+    return `${celciusTemp}\u00B0C is converted to ferhenit and its value is ${ferheneit}\u00B0 F`;
+};
+console.log(convertCelsiusToFahrenheit(32));
+console.log("Task - 3 (Scenario 1 - Modify Array with Methods");
+const myFruitsArray = ["apples", "banana", "oranges"];
+console.log(myFruitsArray);
+myFruitsArray.push("kiwi", "pineapple");
+console.log(myFruitsArray);
+myFruitsArray.pop();
+console.log(myFruitsArray);
+let fruitsfirstitem = myFruitsArray.shift();
+console.log(fruitsfirstitem);
+console.log(myFruitsArray);
+myFruitsArray.unshift("grapes");
+console.log(myFruitsArray);
+console.log("Task - 3 (Scenario 2 - Subarray Creation");
+// Splice & Slice Methods
+console.log("splice & slice methods");
+let myVegetables = ["tomato", "potato", "ginger", "lemon"];
+// Splice Method
+// Splice method basically acceps three arguments, first indiates the start of splice and the second argument to mention the index upto which we want to delete the items. Third argument is used to indicate any other items which we want to add in the original Array.
+let newArr = myVegetables.splice(0, 2, "chilies", "mint");
+// removed first two items from the Array, saved them in new Array new newArr and instead of first two items added other two items i.e. chilies and mint in the original Array
+console.log(myVegetables);
+console.log(newArr);
+console.log("slice");
+// Slice is basically used to extract some infromation from an array without modifying the original Array
+const mystudents = ["Fahad", "Abdullah", "Rohan", "Irum", "Sana", "Komal"];
+// it accepts 3x arguments, start, end and string,
+let sliced = mystudents.slice(2);
+let firstTwosliced = mystudents.slice(0, 2);
+console.log(mystudents);
+console.log(sliced);
+console.log(firstTwosliced);
